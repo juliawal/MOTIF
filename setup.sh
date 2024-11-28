b47dad9379a958c50cc69d79bfe29d139911733c
Julia <115322366+juliawal@users.noreply.github.com>
2024-11-28 18:07:04 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-# Set the necessary environment variable for scikit-learn
-export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
-
-# Install the packages listed in requirements.txt
-pip install -r requirements.txt
